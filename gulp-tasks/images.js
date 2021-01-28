cfb285af0a74bcd7a2ef8f09abfc151fda8d9922
jgdomingues <joaogarabelli@gmail.com>
2021-01-28 13:19:44 -0300

    starter files

---
+++ gulp-tasks/images.js
@@ -0,0 +1,20 @@
+const { dest, src } = require('gulp')
+const imagemin = require('gulp-imagemin')
+
+// Grabs all images, runs them through imagein and plops them in the dist folder
+const images = () => {
+  // We have spcefic configs for jpeg and png files to try to really pull down asset sizes
+  return src('./src/images/**/*')
+    .pipe(
+      imagemin(
+        [
+          imagemin.mozjpeg({ quality: 60, progressive: true }),
+          imagemin.optipng({ optimizationLevel: 5, interlaced: null }),
+        ],
+        { silent: true },
+      ),
+    )
+    .pipe(dest('./src/assets/images'))
+}
+
+module.exports = images
---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './src/App'
+import './src/assets/css/global.css'
+
+ReactDOM.render(<App />, document.getElementById('app'))
